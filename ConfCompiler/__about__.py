83533afd94b63c30ffcaac946bc361607cbf1a57
Reece Harris <reece.harris98@protonmail.com>
2021-07-31 00:15:58 +0100

    Add files via upload

---
+++ ConfCompiler/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+]
+
+__title__ = "confcompiler"
+__summary__ = (
+    "Compile, Read and update your .conf file in python"
+)
+__uri__ = "https://github.com/NotReeceHarris/DotConfCompiler"
+
+__version__ = "0.0.1"
+
+__author__ = "NotReeceHarris (Reece Harris)"
+__email__ = "reece.harris98@protonmail.com"
+
+__license__ = "Apache-2.0 License"
