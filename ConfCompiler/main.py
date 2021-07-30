0e3137422cc6924effaa88b957c2376f11216625
Reece Harris <reece.harris98@protonmail.com>
2021-07-30 23:36:13 +0100

    Update main.py

--- ConfCompiler/main.py
+++ ConfCompiler/main.py
@@ -8,6 +8,24 @@
 #     Github: https://github.com/NotReeceHarris/DotConfCompiler
 #     Dev   : https://github.com/NotReeceHarris
 
+# Config file:
+#       Dont directly edit this file unless you know what your doing,
+#       Editing this file may cause errors and issues later on.
+#
+#       Editing this file directly may also cause security issues
+#       if someone told you to edit this THINK FIRST before editing
+#       this may De-anonymiseing you or open you to data leaks.
+# 
+# Config syntax:
+#       Commenting - All comments must start with '#' and must be on
+#                    there on line, you cannot comment a line with 
+#                    data involved.
+#
+#       Variables  - Data must start with a variable name then continued
+#                    with '=' after that the data.
+#     
+#       Data Types - Boolen, String
+
 def ConfRead(FilePath):
 
     # Creates a dict of all lines in conf file.
